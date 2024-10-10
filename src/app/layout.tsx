19f75d72b22a140edae2a610a6102ffedfdf55c5
4F24L <afzalmir2003@gmail.com>
2024-10-10 12:30:20 +0530

    Landing Page overflow fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.className} style={{ overflowX: 'hidden' }}>
         {children}
         <Analytics />
       </body>

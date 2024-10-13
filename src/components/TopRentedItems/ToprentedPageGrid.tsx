e8de203139fd8cdc903aa0427d8066e25d2b7678
Shivansh <shivanshsaxena2910@gmail.com>
2024-10-14 04:40:13 +0530

    resolved deployment issue

--- src/components/TopRentedItems/ToprentedPageGrid.tsx
+++ src/components/TopRentedItems/ToprentedPageGrid.tsx
@@ -175,7 +175,7 @@ const TopRentedGrid = () => {
     <div className="grid grid-cols-3 gap-5">
       {
       AllTopRentedItems.map((item) => (
-        <div className="p-10">
+        <div key={item.itemName} className="p-10">
           <Card
             image={item.image}
             itemName={item.itemName}
